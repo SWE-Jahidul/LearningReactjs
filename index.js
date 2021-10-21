1ca29915dda82a7a59a37d01136baca3c5626788
SWE-Jahidul <programmerjahid162@gmail.com>
2021-10-21 15:02:28 +0600

    Create dynamic api, api parameter, access params

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 
-app.get('/',(req,res) =>{
-    res.send("helo from node ")
+
+const users = [
+
+    {id :1, name:"jahid" , email : "test@gmail.com" , phone : "01794895700"},
+    {id :2, name:"jahid" , email : "test@gmail.com" , phone : "01794895700"},
+    {id :3, name:"jahid" , email : "test@gmail.com" , phone : "01794895700"},
+    {id :4, name:"jahid" , email : "test@gmail.com" , phone : "01794895700"},
+    {id :5, name:"jahid" , email : "test@gmail.com" , phone : "01794895700"},
+    {id :6, name:"jahid" , email : "test@gmail.com" , phone : "01794895700"},
+
+
+]
+
+app.get('/users',(req,res) =>{
+   res.send(users)
+});
+
+
+app.get('/users/:id',(req,res) =>{
+    const id  = req.params.id;
+    const user = users[id];
+    res.send(user)
 });
 
 
